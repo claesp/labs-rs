15650b87bf4ac8f6054e8f0d073de9b80005a6ea
Claes Persson <c@cp.se>
2023-12-17 18:38:47 +0100

    Adding actix-web as a dependency.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("labs.cx.se");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "labs.cx.se"
+}
+
+#[get("/tools")]
+async fn tools() -> impl Responder {
+    "labs.cx.se/tools"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index).service(tools))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
